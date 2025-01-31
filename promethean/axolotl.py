ac9197aa837fbefab89783affbfb53e058fb75c6
Matt Baker <matthew.reiss.baker@gmail.com>
2025-01-30 18:50:09 -0800

    Generate axolotl configs

---
+++ promethean/axolotl.py
@@ -0,0 +1,120 @@
+from typing import Sequence, TypedDict
+from dataclasses import dataclass, field, asdict
+import yaml
+import os
+from .datasets import HubDataset, JsonlDataset
+
+@dataclass
+class AxolotlDataset:
+    path: str = "/workspace/train.jsonl"
+    ds_type: str = "json"
+    split: str = "train"
+    type: str = "chat_template"
+
+class FsdpConf(TypedDict):
+    fsdp_limit_all_gathers: bool
+    fsdp_sync_module_states: bool
+    fsdp_offload_params: bool
+    fsdp_use_orig_params: bool
+    fsdp_cpu_ram_efficient_loading: bool
+    fsdp_auto_wrap_policy: str
+    fsdp_transformer_layer_cls_to_wrap: str
+    fsdp_state_dict_type: str
+    fsdp_sharding_strategy: str
+
+def default_fsdp_conf() -> FsdpConf:
+    return {
+        "fsdp_limit_all_gathers": True,
+        "fsdp_sync_module_states": True,
+        "fsdp_offload_params": True,
+        "fsdp_use_orig_params": False,
+        "fsdp_cpu_ram_efficient_loading": True,
+        "fsdp_auto_wrap_policy": "TRANSFORMER_BASED_WRAP",
+        "fsdp_transformer_layer_cls_to_wrap": "LlamaDecoderLayer",
+        "fsdp_state_dict_type": "FULL_STATE_DICT",
+        "fsdp_sharding_strategy": "FULL_SHARD",
+    }
+
+@dataclass
+class Config:
+    base_model: str
+    lora_r: int
+    lora_alpha: int
+    lora_dropout: float
+    gradient_accumulation_steps: int
+    micro_batch_size: int
+    num_epochs: int
+    learning_rate: float
+    warmup_steps: int
+    evals_per_epoch: int
+
+@dataclass
+class FullConfig(Config):
+    datasets: Sequence[AxolotlDataset]
+    test_datasets: Sequence[AxolotlDataset]
+
+    # optionally might have model_type or tokenizer_type
+    model_type: str = "LlamaForCausalLM"
+    tokenizer_type: str = "AutoTokenizer"
+
+    load_in_8bit: bool = False
+    load_in_4bit: bool = False
+    strict: bool = False
+
+    dataset_prepared_path: str = "last_run_prepared"
+
+    adapter: str = "lora"
+
+    sequence_len: int = 8192
+    sample_packing: bool = False
+    pad_to_sequence_len: bool = True
+
+    lora_target_linear: bool = True
+
+    optimizer: str = "adamw_torch_fused"
+    lr_scheduler: str = "linear"
+
+    train_on_inputs: bool = False
+    group_by_length: bool = False
+    bf16: str = "auto"
+    tf32: bool = False
+
+    gradient_checkpointing: bool = True
+    gradient_checkpointing_kwargs: dict[str, bool] = field(default_factory=lambda: {
+        "use_reentrant": True
+    })
+    logging_steps: int = 1
+    flash_attention: bool = True
+
+    saves_per_epoch: int = 1
+    weight_decay: float = 0.0
+    fsdp: Sequence[str] = field(default_factory=lambda: [
+        "full_shard",
+        "auto_wrap",
+    ])
+    fsdp_config: FsdpConf = field(default_factory=default_fsdp_conf)
+    special_tokens: dict[str, str] = field(default_factory=lambda: {
+        "pad_token": "<|end_of_text|>"
+    })
+
+def config(datasets: Sequence[HubDataset | JsonlDataset], config: Config):
+    return FullConfig(
+        base_model=config.base_model,
+        datasets=[],
+        test_datasets=[],
+        lora_r=config.lora_r,
+        lora_alpha=config.lora_alpha,
+        lora_dropout=config.lora_dropout,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        micro_batch_size=config.micro_batch_size,
+        num_epochs=config.num_epochs,
+        learning_rate=config.learning_rate,
+        warmup_steps=config.warmup_steps,
+        evals_per_epoch=config.evals_per_epoch,
+    )
+
+def save(output_dir: str, config: FullConfig):
+    file_contents = yaml.dump(asdict(config), default_flow_style=False)
+    path = os.path.join(output_dir, "axolotl.yaml")
+    with open(path, "w") as f:
+        f.write(file_contents)
--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -2,8 +2,10 @@ from promethean.datasets import HubDataset, HubSplit
 from promethean.extract import (
     extract_training_data, GenerationConfig, ClientOpts
 )
+import promethean.axolotl as axolotl
 import os
 
+output_dir="output"
 datasets = [
     HubDataset(
         name="mlabonne/harmless_alpaca",
@@ -15,13 +17,30 @@ datasets = [
     ),
 ]
 
-extract_training_data(GenerationConfig(
-    datasets=datasets,
-    teacher="hf:meta-llama/Meta-Llama-3.1-8B-Instruct",
-    request_batch_size=8,
-    output_dir="output",
-    client_opts=ClientOpts(
-        base_url="https://glhf.chat/api/openai/v1",
-        api_key=os.environ["GLHF_API_KEY"],
-    ),
-))
+def extract():
+    extract_training_data(GenerationConfig(
+        datasets=datasets,
+        teacher="hf:meta-llama/Meta-Llama-3.1-8B-Instruct",
+        request_batch_size=8,
+        output_dir=output_dir,
+        client_opts=ClientOpts(
+            base_url="https://glhf.chat/api/openai/v1",
+            api_key=os.environ["GLHF_API_KEY"],
+        ),
+    ))
+
+def gen_axolotl():
+    axolotl.save(output_dir, axolotl.config(datasets, axolotl.Config(
+        base_model="unsloth/Meta-Llama-3.1-70B-Instruct",
+        lora_r=32,
+        lora_alpha=64,
+        lora_dropout=0.01,
+        gradient_accumulation_steps=4,
+        micro_batch_size=2,
+        num_epochs=10,
+        learning_rate=4e-4,
+        warmup_steps=10,
+        evals_per_epoch=1,
+    )))
+
+gen_axolotl()
